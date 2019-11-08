fc6dc7838f350c0ae4f80c26740022c552f42a9e
melvin-leo <mel5469leo@hotmail.com>
2019-11-08 14:06:25 +0800

    Added neg test train

--- test_hog.py
+++ test_hog.py
@@ -5,13 +5,9 @@ import time
 import cv2
 import os.path as osp
 import numpy as np
-from scipy.spatial.distance import cdist
 from sklearn.svm import LinearSVC, SVC
 from utils import load_image_gray
 import pickle
-import math
-import xml.dom.minidom
-import random
 from skimage import feature
 import os
 
@@ -21,80 +17,13 @@ if __name__ == "__main__":
     #test_image_feats = bags_of_sifts(test_image_paths, vocab_filename)
     with open('finalized_model.pkl', 'rb') as f:
         model = pickle.load(f)
-    # image = load_image_gray('datasets/JPEGImages/003.jpg')
-    # for image in os.listdir(dataset_dir):
-    #     image = cv2.imread(os.path.join(dataset_dir, image))
-    #     image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-    #     # cv2.imshow('og', image)
-    #     # cv2.waitKey(0)
-    #     (winW, winH) = (16, 16)
-    #     # hog = cv2.HOGDescriptor()
-    #     # loop over the image pyramid
-    #     for resized in pyramid(image, scale=1.5):
-    #         # loop over the sliding window for each layer of the pyramid
-    #         for (x, y, window) in sliding_window(resized, stepSize=32, windowSize=(winW, winH)):
-    #             # if the window does not meet our desired window size, ignore it
-    #             if window.shape[0] != winH or window.shape[1] != winW:
-    #                 continue
-    #
-    #             # THIS IS WHERE YOU WOULD PROCESS YOUR WINDOW, SUCH AS APPLYING A
-    #             # MACHINE LEARNING CLASSIFIER TO CLASSIFY THE CONTENTS OF THE
-    #             # WINDOW
-    #
-    #             # since we do not have a classifier, we'll just draw the window
-    #             clone = resized.copy()
-    #             cut = clone[y: y+winH, x: x+winW]
-    #             cut = cv2.resize(cut, (32, 32))
-    #             (H, hogImage) = feature.hog(cut, orientations=9, pixels_per_cell=(4, 4),
-    #                                     cells_per_block=(2, 2), transform_sqrt=True, block_norm="L1", visualize=True)
-    #             test_image_feat = H.flatten()
-    #             from skimage import exposure
-    #             hogImage = exposure.rescale_intensity(hogImage, out_range=(0, 255))
-    #             hogImage = hogImage.astype("uint8")
-    #
-    #             # cv2.imshow('imgagga', hogImage)
-    #             # cv2.waitKey()
-    #
-    #             test_label = model.predict([test_image_feat])
-    #             # print(test_label, y, x)
-    #             if test_label == 'waldo':
-    #                 print(y)
-    #                 print(x)
-    #                 cv2.imshow('imgagga', cut)
-    #                 cv2.waitKey()
-    #                 cv2.imshow('imgagga', hogImage)
-    #                 cv2.waitKey()
-    #
-    #             if test_label == 'wendy':
-    #                 print(y)
-    #                 print(x)
-    #                 cv2.imshow('imgagga', cut)
-    #                 cv2.waitKey()
-    #                 cv2.imshow('imgagga', hogImage)
-    #                 cv2.waitKey()
-    #
-    #             if test_label == 'wizard':
-    #                 print(y)
-    #                 print(x)
-    #                 cv2.imshow('imgagga', cut)
-    #                 cv2.waitKey()
-    #                 cv2.imshow('imgagga', hogImage)
-    #                 cv2.waitKey()
-    #
-
-
-
-
-
-
-
 
     for image in os.listdir('test_images'):
         print(image)
         image = cv2.imread(os.path.join('test_images', image))
         cut = cv2.resize(image, (50, 50))
         (H, hogImage) = feature.hog(cut, orientations=9, pixels_per_cell=(4, 4),
-                                cells_per_block=(2, 2), transform_sqrt=True, block_norm="L1", visualize=True)
+                                cells_per_block=(2, 2), transform_sqrt=True, block_norm="L2", visualize=True)
         test_image_feat = H.flatten()
         from skimage import exposure
         hogImage = exposure.rescale_intensity(hogImage, out_range=(0, 255))
@@ -114,10 +43,10 @@ if __name__ == "__main__":
             cv2.imshow('imgagga', hogImage)
             cv2.waitKey()
 
-        if test_label == 'wendy':
+        if test_label == 'wenda':
             # print(y)
             # print(x)
-            print('wendy_found')
+            print('wenda_found')
             cv2.imshow('imgagga', cut)
             cv2.waitKey()
             cv2.imshow('imgagga', hogImage)
@@ -127,6 +56,15 @@ if __name__ == "__main__":
             # print(y)
             # print(x)
             print('wizard_found')
+            cv2.imshow('imgagga', cut)
+            cv2.waitKey()
+            cv2.imshow('imgagga', hogImage)
+            cv2.waitKey()
+
+        if test_label == 'None':
+            # print(y)
+            # print(x)
+            print('None')
             cv2.imshow('imgagga', cut)
             cv2.waitKey()
             cv2.imshow('imgagga', hogImage)
--- train_hog.py
+++ train_hog.py
@@ -3,15 +3,11 @@ from utils import sliding_window
 import argparse
 import time
 import cv2
-import os.path as osp
 import numpy as np
-from scipy.spatial.distance import cdist
 from sklearn.svm import LinearSVC, SVC
 from utils import load_image_gray
 import pickle
-import math
-import xml.dom.minidom
-import random
+
 
 from skimage import exposure
 from skimage import feature
@@ -19,33 +15,6 @@ import os
 
 train_data_dir = 'trainingdata'
 
-
-# def build_bag_of_hog(image_dict):
-#
-#     print("Start Feats")
-#     feats = []
-#     print(len(image_dict))
-#     for image in image_dict:
-#         # print(image)
-#         image_path = 'datasets/JPEGImages/' + image['index'] + '.jpg'
-#         img = load_image_gray(image_path)
-#         # img = cv2.imread(image_path)
-#         # img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-#         # images[index] = img
-#
-#         img = img[int(image['ymin']):int(image['ymax']), int(image['xmin']):int(image['xmax'])]
-#         if image['name'] == 'waldo' or image['name'] == 'wenda' or image['name'] == 'wizard':
-#             cv2.imshow('imjsdkasg', img)
-#             cv2.waitKey()
-#         img = cv2.resize(img, (32, 32), interpolation = cv2.INTER_AREA)
-#
-#         descriptor = feature.hog(img, orientations=9, pixels_per_cell=(4, 4),
-#                                     cells_per_block=(2, 2), transform_sqrt=True, block_norm="L1")
-#         descriptor = descriptor.flatten()
-#         feats.append(descriptor)
-#         print(image['index'] + ' ' + 'Done')
-#
-#     return feats
 
 def build_bag_of_hog(image_list):
 
@@ -67,7 +36,7 @@ def build_bag_of_hog(image_list):
         img = cv2.resize(img, (50, 50), interpolation = cv2.INTER_AREA)
 
         descriptor = feature.hog(img, orientations=9, pixels_per_cell=(4, 4),
-                                    cells_per_block=(2, 2), transform_sqrt=True, block_norm="L1")
+                                    cells_per_block=(2, 2), transform_sqrt=True, block_norm="L2")
         descriptor = descriptor.flatten()
         feats.append(descriptor)
         print(image_path + ' ' + 'Done')
@@ -76,49 +45,6 @@ def build_bag_of_hog(image_list):
 
 
 if __name__ == "__main__":
-    # train_lists_filename = 'datasets/ImageSets/train.txt'
-    # annotation_path = 'datasets/Annotations/'
-    # check_list = ['xmin', 'xmax', 'ymin', 'ymax']
-    # train_image_list = []
-    # with open(train_lists_filename, "r") as f:
-    #     train_list = f.readlines()
-    # for train_index in train_list:
-    #     specified_region = []
-    #     train_index = train_index.strip()
-    #     dom = xml.dom.minidom.parse(annotation_path+train_index+'.xml')
-    #     root = dom.documentElement
-    #     size = root.getElementsByTagName('size')[0]
-    #     width = int(size.getElementsByTagName('width')[0].firstChild.nodeValue)
-    #     height = int(size.getElementsByTagName('height')[0].firstChild.nodeValue)
-    #     objs = root.getElementsByTagName('object')
-    #
-    #     for obj in objs:
-    #         name = obj.getElementsByTagName('name')[0].firstChild.nodeValue
-    #         bndbox = obj.getElementsByTagName('bndbox')[0]
-    #         key_dict = {}
-    #         for check_coord in check_list:
-    #             coord = int(bndbox.getElementsByTagName(check_coord)[0].firstChild.nodeValue)
-    #             key_dict[check_coord] = coord
-    #         specified_region.append(key_dict)
-    #         key_dict['index'] = train_index
-    #         key_dict['name'] = name
-    #         train_image_list.append(key_dict)
-    #
-    #     time = 10 # may need to find out how many NON per image do we need
-    #     while time > 0:
-    #         xmin = random.randint(0, width - 50)
-    #         xmax = random.randint(xmin + 50, width)
-    #         ymin = random.randint(0, height - 50)
-    #         ymax = random.randint(ymin + 50, height)
-    #         flag = True
-    #         for region in specified_region:
-    #             if not (region['xmax'] < xmin or region['ymax'] < ymin or xmax < region['xmin'] or ymax < region['ymin']) :
-    #                 flag = False
-    #                 break
-    #         if flag:
-    #             key_dict = {'index': train_index, 'name': 'NON', 'xmax': xmax, 'xmin': xmin, 'ymax': ymax, 'ymin':ymin}
-    #             train_image_list.append(key_dict)
-    #             time -=
     train_image_list = []
     train_image_labels = []
     for image in os.listdir(train_data_dir):
@@ -128,23 +54,9 @@ if __name__ == "__main__":
         label = tokens[0]
         if label == 'wendy':
             label = 'wenda'
+        if label == 'neg':
+            label = 'None'
         train_image_labels.append(label)
-
-    print(train_image_list)
-    print(train_image_labels)
-
-
-
-    # train_tag_labels = [image['name'] for image in train_image_list]
-    # print(len(train_image_list))
-    vocab_filename = 'vocab.pkl'
-    train_feats_filename = 'train_feats.pkl'
-    train_tag_filename = 'train_tag.pkl'
-    images = {}
-    # train_image_set = set()
-    # for image_dict in train_image_list:
-    #     if image_dict['name'] == 'waldo' or image_dict['name'] == 'wenda' or image_dict['name'] == 'wizard':
-    # print(train_image_list)
 
     features = build_bag_of_hog(train_image_list)
     features = np.array(features)
